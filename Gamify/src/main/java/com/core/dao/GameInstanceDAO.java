aa41a54817045e537b870aa6f0be249693df41e4
Piyush Rungta <ricky.rungta@gmail.com>
2015-07-22 16:33:43 +0530

    Added Leader Board API

--- Gamify/src/main/java/com/core/dao/GameInstanceDAO.java
+++ Gamify/src/main/java/com/core/dao/GameInstanceDAO.java
@@ -1,10 +1,15 @@
 package com.core.dao;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.core.api.beans.TotalNumberOfGameWonByAUser;
 import com.core.dao.generic.GenericRepository;
 import com.core.domain.knockout.GameInstance;
 
 public interface GameInstanceDAO extends
 		GenericRepository<GameInstance, Serializable> {
+	
+	public List<TotalNumberOfGameWonByAUser> getTopFivePersonWhoWonMaxGames();
 
 }
--- Gamify/src/main/java/com/core/dao/impl/GameInstanceDAOImpl.java
+++ Gamify/src/main/java/com/core/dao/impl/GameInstanceDAOImpl.java
@@ -1,14 +1,31 @@
 package com.core.dao.impl;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
+
+import com.core.api.beans.TotalNumberOfGameWonByAUser;
 import com.core.dao.GameInstanceDAO;
 import com.core.dao.generic.HibernateGenericRepository;
+import com.core.domain.User;
 import com.core.domain.knockout.GameInstance;
 
 @Repository("gameInstanceDAO")
 public class GameInstanceDAOImpl extends
 		HibernateGenericRepository<GameInstance, Serializable> implements
 		GameInstanceDAO {
-
+	
+	public List<TotalNumberOfGameWonByAUser> getTopFivePersonWhoWonMaxGames() {
+		Query query = getSession().createQuery(
+				" select gameWinner as winner, count(*) as noOfWins from GameInstance gi where gi.gameWinner is not null group by gi.gameWinner order by noOfWins desc  limit 5");
+		List<Object[]> listResult = query.list();
+		 List<TotalNumberOfGameWonByAUser> winningUsers = new LinkedList<TotalNumberOfGameWonByAUser>();
+		for (Object[] aRow : listResult) {
+			winningUsers.add(new TotalNumberOfGameWonByAUser((User)aRow[0], (Long)aRow[1]));
+		}
+		return winningUsers;
+	}
 }
